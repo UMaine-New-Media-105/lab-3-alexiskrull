c2a7b85203beba840cfea2d0013b581645ad9a96
alexiskrull <124173712+alexiskrull@users.noreply.github.com>
2023-02-15 09:56:18 -0500

    candy with if/else fills

---
+++ firstExercise.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  if (mouseX < 100){
+    fill("powderblue");
+  } else if (mouseX >= 100 && mouseX < 200){
+    fill("springgreen");
+  } else if (mouseX >= 200 && mouseX < 300){
+    fill("plum");
+  } else {
+    fill("hotpink");
+  }
+  stroke("white");
+  triangle(0, 0, 0, 50, 50, 25);
+  triangle(100, 0, 100, 50, 50, 25);
+  ellipse(50, 25, 50);
+  
+}
